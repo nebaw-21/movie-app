1d1ce4e51d626c5a20dfa7ac8a92247644580931
Nebiyu Zewge <nebiyuzewge@gmail.com>
2024-12-28 11:41:48 +0300

    add notification button

--- src/Components/nav-bar.jsx
+++ src/Components/nav-bar.jsx
@@ -1,23 +1,23 @@
 import {
-    Navbar,
-    Typography,
-    IconButton,
- 
-  } from "@material-tailwind/react";
-  import { StarIcon } from "@heroicons/react/24/solid";
-  import { Link } from "react-router-dom";
-  import Search from "./Search";
-   
-  export default function NavBar() {
-    return (
-      <Navbar
-        variant="gradient"
-        color="blue-gray"
-        fullWidth="true"
-        className="  mx-auto w-full from-blue-gray-900 to-blue-gray-800  "
-      >
-        <div className="flex flex-wrap items-center justify-between gap-y-4 text-white">
-          <Link to={"/"}>
+  Navbar,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
+import { StarIcon, BellIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
+import Search from "./Search";
+
+export default function NavBar() {
+  return (
+    <Navbar
+      variant="gradient"
+      color="blue-gray"
+      fullWidth="true"
+      className="mx-auto w-full from-blue-gray-900 to-blue-gray-800"
+    >
+      <div className="flex flex-wrap items-center justify-between gap-y-4 text-white">
+        {/* Logo Section */}
+        <Link to={"/"}>
           <Typography
             as="a"
             variant="h6"
@@ -25,20 +25,26 @@ import {
           >
             Awra Movie
           </Typography>
-          </Link>
-          <div className="ml-auto mr-3  flex gap-1 md:mr-4">
+        </Link>
+
+        {/* Icons Section */}
+        <div className="ml-auto mr-3 flex gap-3 md:mr-4">
+          {/* Favorite Button */}
           <Link to={"/favorite"}>
             <IconButton variant="text" color="white">
-              <StarIcon className=" h-5 w-5 md:h-6 md:w-6" />
+              <StarIcon className="h-5 w-5 md:h-6 md:w-6" />
             </IconButton>
-
-
           </Link>
-          </div>
 
-          <Search />
-
+          {/* Notification Button */}
+          <IconButton variant="text" color="white">
+            <BellIcon className="h-5 w-5 md:h-6 md:w-6" />
+          </IconButton>
         </div>
-      </Navbar>
-    );
-  }
+
+        {/* Search Bar */}
+        <Search />
+      </div>
+    </Navbar>
+  );
+}
